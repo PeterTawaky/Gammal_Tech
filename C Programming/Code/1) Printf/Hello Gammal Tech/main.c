dbefa20022947f112a5e5afb599d26b7b363397b
Peter Tawaky <peterayman800@gmail.com>
2023-11-23 15:25:46 +0200

    arranging files

--- C Programming/Code/1) Printf/Hello Gammal Tech/main.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(void){
-    printf("\t\t\t\t\t*******************\n"
-           "\t\t\t\t\t*Hello Gammal Tech*\n"
-           "\t\t\t\t\t*******************\n");
-
-
-
-}
